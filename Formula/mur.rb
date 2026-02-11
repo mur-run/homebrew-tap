c939e2b0d7343c6dc2f5029423ea95d51f521eed
david <karajanchang@gmail.com>
2026-02-11 10:57:31 +0800

    feat: add mur formula v1.4.1

---
+++ Formula/mur.rb
@@ -0,0 +1,29 @@
+class Mur < Formula
+  desc "Invisible continuous learning system for AI coding assistants"
+  homepage "https://github.com/mur-run/mur-core"
+  url "https://github.com/mur-run/mur-core/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "da9db9d1f0493f2c3acd64efe50978e886bc259ee98d0be01faf1b0fb4dbe84a"
+  license "MIT"
+  head "https://github.com/mur-run/mur-core.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.Version=#{version}
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.Commit=#{tap.user}
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.BuildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/mur"
+  end
+
+  def post_install
+    ohai "mur installed! Run 'mur init --hooks' to get started."
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mur version")
+  end
+end
