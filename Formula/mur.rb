2e9188adae7d47e4c242685e3f5f61d44cf978c8
david <karajanchang@gmail.com>
2026-02-25 15:14:13 +0800

    formula: upgrade to mur v2.0.0-alpha.1 (Rust rewrite)
    - Switch from Go to Rust build
    - New repo: mur-run/mur (was mur-run/mur-core)
    - Post-install reminds to run 'mur migrate' for v1 users

--- Formula/mur.rb
+++ Formula/mur.rb
@@ -1,29 +1,24 @@
 class Mur < Formula
   desc "Invisible continuous learning system for AI coding assistants"
-  homepage "https://github.com/mur-run/mur-core"
-  url "https://github.com/mur-run/mur-core/archive/refs/tags/v1.15.0.tar.gz"
-  sha256 "fce88699d7e57acc62609eefd66f4b006c82a615d53ba65740563fa11f70d6ed"
+  homepage "https://github.com/mur-run/mur"
+  url "https://github.com/mur-run/mur/archive/refs/tags/v2.0.0-alpha.1.tar.gz"
+  sha256 "6273fd4315365d9fbab16100a80e715898a4d08d9d8f88839f2c8287ba3b2712"
   license "MIT"
-  head "https://github.com/mur-run/mur-core.git", branch: "main"
+  head "https://github.com/mur-run/mur.git", branch: "main"
 
-  depends_on "go" => :build
+  depends_on "rust" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0"
-    ldflags = %W[
-      -s -w
-      -X github.com/mur-run/mur-core/cmd/mur/cmd.Version=#{version}
-      -X github.com/mur-run/mur-core/cmd/mur/cmd.Commit=#{tap.user}
-      -X github.com/mur-run/mur-core/cmd/mur/cmd.BuildDate=#{time.iso8601}
-    ]
-    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/mur"
+    system "cargo", "build", "--release", "--bin", "mur"
+    bin.install "target/release/mur"
   end
 
   def post_install
-    ohai "mur installed! Run 'mur init --hooks' to get started."
+    ohai "mur v2 installed! Run 'mur init --hooks' to get started."
+    ohai "If upgrading from v1, run 'mur migrate' to convert patterns."
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/mur version")
+    assert_match "mur 2", shell_output("#{bin}/mur --version")
   end
 end
