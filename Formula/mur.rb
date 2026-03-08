6f1cfd6d5a81db0c9df261767f1dc7569ca8466b
david <karajanchang@gmail.com>
2026-03-08 17:13:45 +0800

    mur 2.0.0-alpha.27

--- Formula/mur.rb
+++ Formula/mur.rb
@@ -2,12 +2,12 @@ class Mur < Formula
   desc "Invisible continuous learning system for AI coding assistants"
   homepage "https://github.com/mur-run/mur"
   license "MIT"
-  version "2.0.0-alpha.26"
+  version "2.0.0-alpha.27"
 
   on_macos do
     on_arm do
-      url "https://github.com/mur-run/mur/releases/download/v2.0.0-alpha.26/mur-darwin-arm64.tar.gz"
-      sha256 "10065cc970f82405090313498c862686039655863cb8db70e13cfa6081206e3a"
+      url "https://github.com/mur-run/mur/releases/download/v2.0.0-alpha.27/mur-darwin-arm64.tar.gz"
+      sha256 "6301828d859ccfa2f2cb69a9bd508a13c37877866a3e13e3ac7e8c7d232f4622"
     end
   end
 
