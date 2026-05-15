94ea4f73239ac4cebf1b8a229ca5741232cfadae
david <karajanchang@gmail.com>
2026-05-15 13:40:00 +0800

    chore: bump mur to v2.16.0

--- Formula/mur.rb
+++ Formula/mur.rb
@@ -2,12 +2,12 @@ class Mur < Formula
   desc "Invisible continuous learning system for AI coding assistants"
   homepage "https://github.com/mur-run/mur"
   license "MIT"
-  version "2.15.0"
+  version "2.16.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/mur-run/mur/releases/download/v2.15.0/mur-aarch64-apple-darwin.tar.gz"
-      sha256 "49f122fe4efeb80db58ad426186ac57253127bdbbacabf0238415cb2b1183d88"
+      url "https://github.com/mur-run/mur/releases/download/v2.16.0/mur-aarch64-apple-darwin.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
